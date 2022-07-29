3946f51e31edf3bdcd4b3bbbde70af5330658ab4
Thales Macedo Garitezi <thalesmg@gmail.com>
2022-07-29 17:25:32 -0300

    ci: fix release... again.

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -4,10 +4,18 @@ export MIX_ENV=prod
 
 arch="$1"
 
+apt update
+apt install -yyq git make
+
+mix local.hex --force
+mix local.rebar --force
+mix deps.get
+
 make all
 
-for f in $(ls _build/prod/*.tar.gz)
+for f in $(ls _build/$MIX_ENV/*.tar.gz)
 do
   out=${f%%.tar.gz}-${arch}
+  out=${out##*/}
   mv "$f" ${out}.tar.gz
 done
